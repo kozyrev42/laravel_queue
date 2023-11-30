d8f5b788451a9de798b7bca36c8d0b059bb7fdd8
kozyrev42 <alexander.kozyrev@pluscards.ru>
2023-11-30 13:54:08 +0700

    2. Сделал роут и метод для регистрации юзера.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
 
 - laravel version: 8.83
+- базу данных использую "step3"
 
 1. Инициализация проекта.
+
+2. Сделал роут и метод для регистрации юзера.
---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:3',
+        ]);
+
+        if ($validatedData->fails()) {
+            return response(['errors' => $validatedData->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+
+        return response(['user' => $user], 201);
+    }
+}
